a14b4db523803eb8658f5f1878d1bdb535632cb6
adipinhas <adi9247@gmail.com>
2020-02-25 20:22:40 +0200

    Updates

---
+++ precache-manifest.1dc063e4f9d6ed01e489e6d051cacd9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21f90aa9914c24b66bddf12b75bd27c",
+    "url": "/colors-go/index.html"
+  },
+  {
+    "revision": "227bbac4e93ad2919131",
+    "url": "/colors-go/static/css/2.753ea2eb.chunk.css"
+  },
+  {
+    "revision": "3a830a8e7678010fabc0",
+    "url": "/colors-go/static/css/main.ecea7843.chunk.css"
+  },
+  {
+    "revision": "227bbac4e93ad2919131",
+    "url": "/colors-go/static/js/2.60817655.chunk.js"
+  },
+  {
+    "revision": "9d1d1719818821b9aac3ae9f2463ae75",
+    "url": "/colors-go/static/js/2.60817655.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a830a8e7678010fabc0",
+    "url": "/colors-go/static/js/main.cafd57a1.chunk.js"
+  },
+  {
+    "revision": "e3f769412e3438b947a1",
+    "url": "/colors-go/static/js/runtime-main.847308a1.js"
+  },
+  {
+    "revision": "0c14eab932e562373594452f0599f0f2",
+    "url": "/colors-go/static/media/photo-1513542789411-b6a5d4f31634.0c14eab9.jpg"
+  }
+]);
